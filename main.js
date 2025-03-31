2adaa2c3c8f8fac4f5df0d6c26b49cd9cc1c4238
enzoflashiron <enzoflashiron@gmail.com>
2025-03-31 23:45:07 +0000

    função para receber o valor da conta (bill) e função para receber o número de pessoas

---
+++ main.js
@@ -0,0 +1,14 @@
+let bill = 0;
+let tipPercentage = 0;
+let numberOfPeople = 0;
+
+function ReceiveBillValue(){
+   bill = document.querySelector("#bill").valueAsNumber
+
+}
+
+function ReceiveNumberOfPeopleValue(){
+   numberOfPeople = document.querySelector("#people").valueAsNumber
+
+}
+
